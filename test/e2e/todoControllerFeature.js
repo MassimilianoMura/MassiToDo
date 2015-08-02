d6b8641e973961b9bdfc7ca51da6eb35b463bd92
MassimilianoMura <massimilianomura1983@gmail.com>
2015-08-02 18:03:06 +0100

    adding test for checkbox, to mark task as done

--- test/e2e/todoControllerFeature.js
+++ test/e2e/todoControllerFeature.js
@@ -1,8 +1,8 @@
 describe('Massi todo app', function() {
 
   var taskBox = element(by.model('newTodoDescription'));
-
-  var list =element.all(by.repeater('task in tasks'));
+  var list = element.all(by.repeater('task in tasks'));
+  var boxDone = element.all(by.css('checkbox'))
 
   var tasks = {}
 
@@ -15,10 +15,15 @@ describe('Massi todo app', function() {
     expect(browser.getTitle()).toEqual('MassiToDo');
   });
 
-
   it('can add and display a new task', function() {
     taskBox.sendKeys('my new todo').sendKeys(protractor.Key.ENTER);
     expect(list.last().getText()).toEqual('my new todo')
+  });
+
+  it('can be selected as done', function() {
+    taskBox.sendKeys('my new todo').sendKeys(protractor.Key.ENTER);
+    selectCheckbox = boxDone.isSelected();
+    expect(selectCheckbox).toBeTruthy();
   });
 
 
